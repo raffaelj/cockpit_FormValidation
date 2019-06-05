8c6a2ba8a4f34cdea27ea6c04a654d57a5802854
Raffael <github@raffaeljesche.de>
2019-06-05 18:08:46 +0200

    fixed wrong route when creating new form

--- admin.php
+++ admin.php
@@ -2,6 +2,12 @@
 
 // bind custom function form($name) and keep the other functions intact
 
+$app->bind('/forms/form', function($params){
+
+    return $this->invoke('FormValidation\\Controller\\CustomAdmin', 'form');
+
+});
+
 $app->bind('/forms/form/:name', function($params){
 
     return $this->invoke('FormValidation\\Controller\\CustomAdmin', 'form', ['name' => $params['name']]);
