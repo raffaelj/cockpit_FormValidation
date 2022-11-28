4b0020f75b659f850c47b1bfaccc781a52f55a6f
Raffael <github@raffaeljesche.de>
2022-11-28 17:14:33 +0100

    improved default form template

--- templates/emails/contactform.php
+++ templates/emails/contactform.php
@@ -1,24 +1,59 @@
 <?php
+/**
+ * Default email template of the FormValidion addon for Cockpit CMS v1
+ */
 
-// map field labels to data names
-$out = cockpit('formvalidation')->nameToLabel($data, $frm);
+$formFields = [];
+if (!empty($frm['fields']) && is_array($frm['fields'])) {
+    foreach ($frm['fields'] as $field) {
+        $formFields[$field['name']] = $field;
+    }
+}
 
 // templating in email_text_after and email_text_before
-$email_text_before = isset($frm['email_text_before']) && !empty($frm['email_text_before']) ? cockpit('formvalidation')->map($frm['email_text_before'], $data) : false;
-
-$email_text_after = isset($frm['email_text_after']) && !empty($frm['email_text_after']) ? cockpit('formvalidation')->map($frm['email_text_after'], $data) : false;
+$email_text_before = isset($frm['email_text_before']) && !empty($frm['email_text_before'])
+    ? cockpit('formvalidation')->map($frm['email_text_before'], $data) : false;
+$email_text_after  = isset($frm['email_text_after']) && !empty($frm['email_text_after'])
+    ? cockpit('formvalidation')->map($frm['email_text_after'], $data) : false;
 
 ?>
 <!DOCTYPE HTML>
 <html><head><meta charset="utf-8" /></head><body>
-<?php if ($email_text_before): ?>
-<p>{{ $email_text_before }}</p>
-<?php endif; ?>
-<?php foreach ($out as $field => $val): ?>
-<p><strong>{{ $field }}:</strong><br />
-{{ (is_string($val) ? $val : json_encode($val)) }}</p>
-<?php endforeach;?>
-<?php if ($email_text_after): ?>
-<p>{{ $email_text_after }}</p>
-<?php endif; ?>
+@if($email_text_before)<p>{{ $email_text_before }}</p>@endif
+
+@foreach($data as $k => $val)
+<p><strong>{{ !empty($formFields[$k]['label']) ? htmlspecialchars($formFields[$k]['label']) : $k }}:</strong><br />
+@if(isset($formFields[$k]))
+    @if($formFields[$k]['type'] == 'multipleselect' || $formFields[$k]['type'] == 'select')
+        @if(is_array($val))
+            @foreach($val as $opt)
+            {{ $formFields[$k]['options']['options'][$opt] ?? $opt }}<br />
+            @endforeach
+        @else
+        {{ is_string($val) ? ($formFields[$k]['options']['options'][$val] ?? $val) : json_encode($val) }}
+        @endif
+    @elseif($formFields[$k]['type'] == 'file')
+        @if(is_array($val))
+            @foreach($val as $url)
+                @if(is_string($url))
+                    {{ $url }}<br />
+                @elseif(is_array($url))
+                    @foreach($url as $v)
+                        {{ is_string($v) ? $v : json_encode($v) }}<br />
+                    @endforeach
+                @endif
+            @endforeach
+        @else
+            {{ $val }}
+        @endif
+    @else
+        {{ is_string($val) ? $val : json_encode($val) }}
+    @endif
+@else
+{{ is_string($val) ? $val : json_encode($val) }}
+@endif
+</p>
+@endforeach
+
+@if($email_text_after)<p>{{ $email_text_after }}</p>@endif
 </body></html>
